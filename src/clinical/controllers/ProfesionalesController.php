ec3a34fd819c0b4dcbe3ab474ff140b2da1a3c4c
Nicolás Larena <nic.lar.universidad@gmail.com>
2021-05-19 02:20:09 -0300

    profesionales from database

--- src/clinical/controllers/ProfesionalesController.php
+++ src/clinical/controllers/ProfesionalesController.php
@@ -4,9 +4,23 @@
 namespace src\clinical\controllers;
 
 
+use src\clinical\services\MedicoService;
+
 class ProfesionalesController extends Controller
 {
+
+    protected MedicoService $service;
+
+    public function init()
+    {
+        parent::init();
+        $this->service = new MedicoService($this->connection,$this->logger);
+    }
+
     public function get() : void {
-        $this->pageFinderService->findFileRute("profesionales");
+        $medicos = $this->service->findAll();
+        $this->pageFinderService->findFileRute("profesionales","php","php",["profesionales"],
+        ["medicos"=>$medicos],
+        "Profesionales");
     }
 }
